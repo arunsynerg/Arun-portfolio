3c85e5c1c2b08b6f33066f80627eb099067a2853
ArunKumar <arun@ESTPL.ESSELSYNERG.COM>
2024-01-09 15:58:36 +0530

    merge conflict solved

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import { Link } from "react-scroll"
 import contactimg from "../assets/contact.png"
+import { AiOutlineClose } from "react-icons/ai";
 import { FaBars } from "react-icons/fa";
 import { FaRegMessage } from "react-icons/fa6";
 
 import "./navbar.css"
 function Navbar() {
     const [showmenu,setShowmenu] = useState(false);
+    
     return (
         <div className='navcontainer'>
             <nav className='navbar'>
@@ -20,7 +22,8 @@ function Navbar() {
                     {/* <Link activeClassName='active'  to='Contactcontainer' smooth={true} duration={500} className="navbarmenitemlist">Contact</Link> */}
                 </div>
                 <Link to='Contactcontainer' smooth={true} duration={500} ><button className='navcontactbtn'><FaRegMessage className='img'/>Contact Me</button></Link>
-                <FaBars className='navbaricon' onClick={()=>setShowmenu(!showmenu)}/>
+                <div className='navicon_container'><FaBars className='navbaricon' onClick={()=>setShowmenu(!showmenu)} style={{display : showmenu ? 'none' : 'block'}}/></div>
+                <AiOutlineClose className='navbaricon_clbtn' onClick={()=>setShowmenu(!showmenu)} style={{display : showmenu ? 'block' : 'none'}}/>
                 <div className='navmoblieitems' style={{display : showmenu ? 'flex' : 'none'}}>
                     <Link activeClassName='active'  to='introcontainer' smooth={true} duration={500}  onClick={()=>setShowmenu(false)} className="navbarmenitemlist">About</Link>
                     <Link activeClassName='active'  to='container' smooth={true} duration={500} offset={-40} onClick={()=>setShowmenu(false)} className="navbarmenitemlist">Skills</Link>
